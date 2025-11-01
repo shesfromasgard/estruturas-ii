ecec7f159d30c186d0a80c8bc6e1519bf61ab32e
laura 🩰 <72207838+shesfromasgard@users.noreply.github.com>
2025-10-31 23:15:09 -0300

    Check for non-diagonal zeros in completeness check

--- Grafos/IsComplete.java
+++ Grafos/IsComplete.java
@@ -18,7 +18,7 @@ public class IsComplete {
 
         for(int i = 0; i < a; ++i)
             for(int j = i + 1; j < a; ++j)
-                if(matrix[i][j] == 0) {
+                if(matrix[i][j] == 0 && i != j) {
                     System.out.println("Not complete");
                     System.exit(0);
                 }
